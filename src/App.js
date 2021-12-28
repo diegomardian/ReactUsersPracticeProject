7d63c8d9034b6b7421b635346292456233cbbc1b
Joe Schlob <jshlob3@gmail.com>
2021-12-28 09:06:08 -0700

    Add login page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 import './App.css';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import UsersTable from './UsersTable';
 import { Routes, Route } from "react-router-dom";
+import Login from './Login';
 
 import UserDetails from './UserDetails';
+import Header from "./Header";
+
 function App() {
   const [users, setUsers] = useState([]);
   const [page, setPage] = useState(1);
+  const [user, setUser] = useState('');
+
+  const logout = () => {
+    window.localStorage.setItem('user', '');
+    setUser('');
+  }
+
+  useEffect(() => {
+    const loggedInUser = window.localStorage.getItem("user");
+    if (loggedInUser) {
+      setUser(loggedInUser);
+    }
+  }, []);
+
+  if (!user) {
+    return <Login setUser={setUser}/>
+  }
 
   return (
-    <div className="App" style={{justifyContent: 'center', display: 'flex'}}>
+    <div className="App">
+      <Header logout={logout}/>
+      <div className="App" style={{justifyContent: 'center', display: 'flex'}}>
         <div style={{width: '80%'}}>
-            <Routes>
-                <Route path="/" element={<UsersTable users={users} setPage={setPage} page={page} setUsers={setUsers}/>} />
-                <Route path="/users/:userId" element={<UserDetails/>} />
-            </Routes>
+          <Routes>
+            <Route path="/" element={<UsersTable users={users} setPage={setPage} page={page} setUsers={setUsers}/>} />
+            <Route path="/users/:userId" element={<UserDetails/>} />
+          </Routes>
         </div>
+      </div>
     </div>
+
   );
 }
 
